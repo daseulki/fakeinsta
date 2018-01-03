16215d37f3690f6ff59e150dab0e2b531c860420
daseulki <sisial1002@gmail.com>
2018-01-03 08:37:58 +0000

    search need fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,57 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+
   def index
-    @post = Post.all
+    @post = Post.where("title LIKE ?", "%#{params["q"]}%")
   end
 
   def new
   end
 
   def create
-    @post = Post.create(
-      title: params[:title],
-      content: params[:content],
-      name: params[:name],
-      postimage: params[:postimage]
-    )
-
-    redirect_to "/"
+    Post.create(post_params)
+    # post = Post.new
+    # post.title = params[:title]
+    # post.content = params[:content]
+    # post.save
+    puts "************************"
+    puts post_params.inspect
+    puts "************************"
+    redirect_to '/'
 
   end
 
   def show
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
 
 
   def update
-    @post = Post.find(params[:id]).update(
-      title: params[:title],
-      content: params[:content],
-      name: params[:name]
+    @post.update(post_params
     )
     redirect_to "/"
   end
 
   def destroy
-    @post = Post.find(params[:id]).destroy
+    @post.destroy
     redirect_to "/"
   end
 
+  # private 위에는 public이라서 url로 요청오면 그것을 받아서 뷰를 보여준다.
+    private
+    # private은 해당 컨트롤러(PostsController)에서만 사용가능함.
+    # 객체에서 private 속성을 가지면.. 클래스 내부에서만 접근이 가능했었고,
+    # 이외의 상속받은 클래스나 객체 인스턴스에서도 접근이 불가능했음.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
-    params.require(:post).permit(:title, :content)
     params.require(:post).permit(:title, :content, :postimage)
 
   end
